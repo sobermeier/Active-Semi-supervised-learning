7c51cf9c77f300c8c38db80af00a7decc33d705e
PhilJahn <jahn@dbs.ifi.lmu.de>
2024-12-30 08:52:52 +0100

    Some dataset fixes

--- semilearn/datasets/cv_datasets/datasetbase.py
+++ semilearn/datasets/cv_datasets/datasetbase.py
@@ -7,9 +7,34 @@ from PIL import Image
 import torchvision
 from torchvision import transforms
 from torch.utils.data import Dataset
+from torchvision.datasets import ImageFolder
 
 from semilearn.datasets.augmentation import RandAugment
 from semilearn.datasets.utils import get_onehot
+
+
+def accimage_loader(path):
+    import accimage
+    try:
+        return accimage.Image(path)
+    except IOError:
+        # Potentially a decoding problem, fall back to PIL.Image
+        return pil_loader(path)
+
+
+def pil_loader(path):
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        return img.convert('RGB')
+
+
+def default_loader(path):
+    from torchvision import get_image_backend
+    if get_image_backend() == 'accimage':
+        return accimage_loader(path)
+    else:
+        return pil_loader(path)
 
 
 class BasicDataset(Dataset):
@@ -29,7 +54,7 @@ class BasicDataset(Dataset):
                  is_ulb=False,
                  strong_transform=None,
                  onehot=False,
-                 *args, 
+                 *args,
                  **kwargs):
         """
         Args
@@ -55,7 +80,7 @@ class BasicDataset(Dataset):
         if self.strong_transform is None:
             if self.is_ulb:
                 assert self.alg not in ['fullysupervised', 'supervised', 'pseudolabel', 'vat', 'pimodel', 'meanteacher', 'mixmatch'], f"alg {self.alg} requires strong augmentation"
-    
+
     def __sample__(self, idx):
         """ dataset specific sample function """
         # set idx-th target
@@ -87,7 +112,7 @@ class BasicDataset(Dataset):
             if not self.is_ulb:
                 if self.alg == 'defixmatch' and self.strong_transform is not None:
                     # NOTE Strong augmentation on the labelled for DeFixMatch
-                    return {'idx_lb': idx, 'x_lb': img_w, 'x_lb_s': self.strong_transform(img), 'y_lb': target} 
+                    return {'idx_lb': idx, 'x_lb': img_w, 'x_lb_s': self.strong_transform(img), 'y_lb': target}
                 else:
                     return {'idx_lb': idx, 'x_lb': img_w, 'y_lb': target}
             else:
@@ -113,6 +138,108 @@ class BasicDataset(Dataset):
                 result['y_lb_al'] = target
                 return result
 
+
+    def __len__(self):
+        return len(self.data)
+
+class LoaderDataset(Dataset):
+    """
+    BasicDataset returns a pair of image and labels (targets).
+    If targets are not given, BasicDataset returns None as the label.
+    This class supports strong augmentation for FixMatch,
+    and return both weakly and strongly augmented images.
+    """
+
+    def __init__(self,
+                 alg,
+                 data,
+                 targets=None,
+                 num_classes=None,
+                 transform=None,
+                 is_ulb=False,
+                 strong_transform=None,
+                 onehot=False,
+                 *args,
+                 **kwargs):
+        """
+        Args
+            data: x_data
+            targets: y_data (if not exist, None)
+            num_classes: number of label classes
+            transform: basic transformation of data
+            use_strong_transform: If True, this dataset returns both weakly and strongly augmented images.
+            strong_transform: list of transformation functions for strong augmentation
+            onehot: If True, label is converted into onehot vector.
+        """
+        super(LoaderDataset, self).__init__()
+        self.alg = alg
+        self.data = data
+        self.targets = targets
+
+        self.loader = default_loader
+
+        self.num_classes = num_classes
+        self.is_ulb = is_ulb
+        self.onehot = onehot
+
+        self.transform = transform
+        self.strong_transform = strong_transform
+        if self.strong_transform is None:
+            if self.is_ulb:
+                assert self.alg not in ['fullysupervised', 'supervised', 'pseudolabel', 'vat', 'pimodel', 'meanteacher',
+                                        'mixmatch'], f"alg {self.alg} requires strong augmentation"
+
+    def __sample__(self, index):
+        path = self.data[index]
+        sample = self.loader(path)
+        target = self.targets[index]
+        return sample, target
+
+    def __getitem__(self, idx):
+        """
+        If strong augmentation is not used,
+            return weak_augment_image, target
+        else:
+            return weak_augment_image, strong_augment_image, target
+        """
+        img, target = self.__sample__(idx)
+
+        if self.transform is None:
+            return {'x_lb': transforms.ToTensor()(img), 'y_lb': target}
+        else:
+            if isinstance(img, np.ndarray):
+                img = Image.fromarray(img)
+            img_w = self.transform(img)
+            if not self.is_ulb:
+                if self.alg == 'defixmatch' and self.strong_transform is not None:
+                    # NOTE Strong augmentation on the labelled for DeFixMatch
+                    return {'idx_lb': idx, 'x_lb': img_w, 'x_lb_s': self.strong_transform(img), 'y_lb': target}
+                else:
+                    return {'idx_lb': idx, 'x_lb': img_w, 'y_lb': target}
+            else:
+                if self.alg == 'fullysupervised' or self.alg == 'supervised':
+                    result = {'idx_ulb': idx}
+                elif self.alg == 'pseudolabel' or self.alg == 'vat':
+                    result = {'idx_ulb': idx, 'x_ulb_w': img_w}
+                elif self.alg == 'pimodel' or self.alg == 'meanteacher' or self.alg == 'mixmatch':
+                    # NOTE x_ulb_s here is weak augmentation
+                    result = {'idx_ulb': idx, 'x_ulb_w': img_w, 'x_ulb_s': self.transform(img)}
+                elif self.alg == 'remixmatch':
+                    rotate_v_list = [0, 90, 180, 270]
+                    rotate_v1 = np.random.choice(rotate_v_list, 1).item()
+                    img_s1 = self.strong_transform(img)
+                    img_s1_rot = torchvision.transforms.functional.rotate(img_s1, rotate_v1)
+                    img_s2 = self.strong_transform(img)
+                    result = {'idx_ulb': idx, 'x_ulb_w': img_w, 'x_ulb_s_0': img_s1, 'x_ulb_s_1': img_s2,
+                              'x_ulb_s_0_rot': img_s1_rot, 'rot_v': rotate_v_list.index(rotate_v1)}
+                elif self.alg == 'comatch':
+                    result = {'idx_ulb': idx, 'x_ulb_w': img_w, 'x_ulb_s_0': self.strong_transform(img),
+                              'x_ulb_s_1': self.strong_transform(img)}
+                else:
+                    result = {'idx_ulb': idx, 'x_ulb_w': img_w, 'x_ulb_s': self.strong_transform(img)}
+                result['x_lb_al'] = img_w
+                result['y_lb_al'] = target
+                return result
 
     def __len__(self):
         return len(self.data)
